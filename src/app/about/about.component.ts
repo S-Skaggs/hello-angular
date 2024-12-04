4d36b1bebf37cd2bdddf0adf7862eb0a3de6b783
S-Skaggs <s-skaggs@outlook.com>
2024-12-04 13:06:49 -0500

    FEAT: Finished chapter 5.

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,16 +1,23 @@
 import { Component } from '@angular/core';
+import { Message } from '../message';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-about',
   standalone: true,
   imports: [],
   template: `
-    <p>
-      about works!
-    </p>
+    <div>
+      <h1>{{ message.title }}</h1>
+      <p>{{ message.content }}</p>
+    </div>
   `,
   styles: ``
 })
 export class AboutComponent {
+  message: Message;
 
+  constructor(private messageService: MessageService) {
+    this.message = messageService.getMessageById(2);
+  }
 }
